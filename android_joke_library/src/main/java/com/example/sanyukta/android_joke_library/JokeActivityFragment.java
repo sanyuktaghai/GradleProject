e55bd321c81c634b0420d6f0b355ccd21a4f8554
sanyuktaghai <sanyukta22@gmail.com>
2016-02-11 20:10:33 -0600

    Add Java Library and Android Library
    Add Java library to supply jokes and Android library to display jokes
    in an activity

---
+++ android_joke_library/src/main/java/com/example/sanyukta/android_joke_library/JokeActivityFragment.java
@@ -0,0 +1,31 @@
+package com.example.sanyukta.android_joke_library;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class JokeActivityFragment extends Fragment {
+
+    public JokeActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_joke, container, false);
+        Intent intent = getActivity().getIntent();
+        String joke = intent.getStringExtra(JokeActivity.JOKE_KEY);
+        TextView jokeTextView = (TextView) root.findViewById(R.id.joke_textview);
+        if (joke != null && joke.length() != 0) {
+            jokeTextView.setText(joke);
+        }
+
+        return root;
+    }
+}
---
+++ java_joke_library/src/main/java/com/example/ClassJoke.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class ClassJoke {
+    public String getJoke(){
+        return "This is a joke from a Java Library";
+    }
+}
